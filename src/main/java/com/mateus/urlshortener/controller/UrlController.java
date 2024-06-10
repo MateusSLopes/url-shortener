af2b717f38b42d451c0256e57ae2a122a7c0bc8d
Mateus Lopes <mateus_tiwu@hotmail.com>
2024-06-10 12:52:09 -0500

    UrlController

--- src/main/java/com/mateus/urlshortener/controller/UrlController.java
+++ src/main/java/com/mateus/urlshortener/controller/UrlController.java
@@ -1,2 +1,36 @@
-package com.mateus.urlshortener.controller;public class UrlController {
+package com.mateus.urlshortener.controller;
+
+import com.mateus.urlshortener.domain.UrlModel;
+import com.mateus.urlshortener.dto.UrlDto;
+import com.mateus.urlshortener.service.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.URL;
+
+@RestController
+public class UrlController {
+    @Autowired
+    UrlService urlService;
+
+    @GetMapping("/{shortUri}")
+    public RedirectView redirectByShortUri(@PathVariable String shortUri) {
+        URL url = urlService.findByShortUri(shortUri);
+        return new RedirectView(url.toString());
+    }
+
+    @PostMapping
+    public ResponseEntity<UrlModel> saveUrl(@RequestBody UrlDto urlDto) {
+        UrlModel urlModel = urlService.save(urlDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(urlModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUrlById(@PathVariable Long id) {
+        urlService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Url deleted successfully");
+    }
 }
--- src/test/java/com/mateus/urlshortener/controller/UrlControllerTest.java
+++ src/test/java/com/mateus/urlshortener/controller/UrlControllerTest.java
@@ -15,12 +15,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
